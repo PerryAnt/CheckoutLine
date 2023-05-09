a1f18c30ea338a3ffe1818b69d42b7e0833de290
PerryAnt <antonelliperry@gmail.com>
2023-05-09 13:55:12 -0500

    Update Memory.tsx
    debugger

--- src/pages/Memory.tsx
+++ src/pages/Memory.tsx
@@ -20,6 +20,7 @@ export default function Memory() {
     newBoard = newBoard.map(
       (value, index) => (index % ((size * size) / 2)) + 1
     );
+    debugger;
     let j = 0;
 
     for (let i = newBoard.length - 1; i > 0; i--) {
