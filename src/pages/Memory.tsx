2a1071d53fba3572c0d0c83ca581ba31c55834fb
PerryAnt <antonelliperry@gmail.com>
2023-05-09 19:42:02 -0500

    Update Memory.tsx
    hopefully fixed useEffect. I believe the issue is returning getNewBoard function instead of running it. So it was running when the component was unmounted instead of when it was mounted which wasn't noticeable when running locally because strict mode mounts, unmounts, then remounts

--- src/pages/Memory.tsx
+++ src/pages/Memory.tsx
@@ -17,10 +17,11 @@ export default function Memory() {
 
   function getNewBoard() {
     let newBoard = Array(size * size).fill(0);
+
     newBoard = newBoard.map(
       (value, index) => (index % ((size * size) / 2)) + 1
     );
-    console.log(newBoard);
+
     let j = 0;
 
     for (let i = newBoard.length - 1; i > 0; i--) {
@@ -88,7 +89,8 @@ export default function Memory() {
     setState(newState);
   }
 
-  useEffect(() => getNewBoard, []);
+  useEffect(() => getNewBoard(), []);
+
   return (
     <div>
       {board.map((row, rowIndex) => (
